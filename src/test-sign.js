6f80ea43c6deb25299ff32801f9afba59b6dad49
Anuchon <anuchon.mpt@gmail.com>
2024-11-16 17:47:26 +0700

    add test sign

--- src/test-sign.js
+++ src/test-sign.js
@@ -1,14 +1,24 @@
 const { SignProtocolClient, EvmChains, SpMode } = require('@ethsign/sp-sdk');
-const { privateKeyToAccount } = require('viem/accounts');
 require('dotenv').config();
 
 async function main() {
-  const privateKey =
-    '2ef78c5b711f4f90d60a79a5ad4e9d9285b292ba47065392f77b27062ae3aee1';
-  const client = new SignProtocolClient(SpMode.OffChain, {
+  const privateKey = '0x1862BDB78aD1F0914aedd3673CC5cCbf561B6f73';
+  const client = new SignProtocolClient(SpMode.OnChain, {
     chain: EvmChains.baseSepolia,
-    account: privateKeyToAccount(privateKey), // Optional, depending on environment
+    account: privateKey, // Optional, depending on environment
   });
+  console.log(client);
+
+  try {
+    const createSchemaRes = await client.createSchema({
+      name: 'xxx',
+      data: [{ name: 'name', type: 'string' }],
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  //
+  // console.log(res);
 }
 
 main();
